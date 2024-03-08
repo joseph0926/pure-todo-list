fa886ce28940e19b9488ad35a209719371856197
김영훈 <joseph0926@naver.com>
2024-03-08 19:20:58 +0900

    기본 설정 구성 (eslint, webpack, jest, typescript, babel)

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-typescript"],
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+    hot: true,
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+  },
+};
